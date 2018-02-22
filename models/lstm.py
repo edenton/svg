a2e2b5058a9429a687f5438f53b952e57fe30672
edenton <denton@cs.nyu.edu>
2018-02-22 15:12:28 -0500

    data root used.

--- models/lstm.py
+++ models/lstm.py
@@ -35,37 +35,39 @@ class lstm(nn.Module):
         return self.output(h_in)
 
 class gaussian_lstm(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size, batch_size):
+    def __init__(self, input_size, output_size, hidden_size, n_layers, batch_size):
         super(gaussian_lstm, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
         self.hidden_size = hidden_size
+        self.n_layers = n_layers
         self.batch_size = batch_size
         self.embed = nn.Linear(input_size, hidden_size)
-        self.lstm = nn.LSTMCell(hidden_size, hidden_size)
+        self.lstm = nn.ModuleList([nn.LSTMCell(hidden_size, hidden_size) for i in range(self.n_layers)])
         self.mu_net = nn.Linear(hidden_size, output_size)
         self.logvar_net = nn.Linear(hidden_size, output_size)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
-        return  (Variable(torch.zeros(self.batch_size, self.hidden_size).cuda()), 
-                 Variable(torch.zeros(self.batch_size, self.hidden_size).cuda()))
-
-
+        hidden = []
+        for i in range(self.n_layers):
+            hidden.append((Variable(torch.zeros(self.batch_size, self.hidden_size).cuda()),
+                           Variable(torch.zeros(self.batch_size, self.hidden_size).cuda())))
+        return hidden
 
     def reparameterize(self, mu, logvar):
-        if self.training:
-            logvar = logvar.mul(0.5).exp_()
-            eps = Variable(logvar.data.new(logvar.size()).normal_())
-            return eps.mul(logvar).add_(mu)
-        else:
-            return mu
+        logvar = logvar.mul(0.5).exp_()
+        eps = Variable(logvar.data.new(logvar.size()).normal_())
+        return eps.mul(logvar).add_(mu)
 
     def forward(self, input):
         embedded = self.embed(input.view(-1, self.input_size))
-        self.hidden = self.lstm(embedded, self.hidden)
-        mu = self.mu_net(self.hidden[0])
-        logvar = self.logvar_net(self.hidden[0])
+        h_in = embedded
+        for i in range(self.n_layers):
+            self.hidden[i] = self.lstm[i](h_in, self.hidden[i])
+            h_in = self.hidden[i][0]
+        mu = self.mu_net(h_in)
+        logvar = self.logvar_net(h_in)
         z = self.reparameterize(mu, logvar)
         return z, mu, logvar
             
